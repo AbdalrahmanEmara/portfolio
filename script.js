0fa9305e6b2884719d743480b9ae9b72755fa8c6
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-08-16 10:42:06 +0300

    Implement Hero Section

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const navContainer = document.querySelector('nav');
 const navLinks = document.querySelectorAll('.nav__link');
 const navLink = document.querySelector('.nav__link');
+const namePopup = document.querySelector(".name__popup");
+const homeIntro = document.querySelector(".home__intro");
 
 function updateNavbarContent() {
   if (window.innerWidth > 600) {
-    navLinks.forEach((link) => link.innerHTML = link.dataset.content)
+    navLinks.forEach((link) => (link.innerHTML = link.dataset.content));
   }
   if (window.innerWidth <= 600) {
-    navLinks.forEach((link) => link.innerHTML = `<i class="${link.dataset.content__mobile}"></i>`);
+    navLinks.forEach(
+      (link) =>
+        (link.innerHTML = `<i class="${link.dataset.content__mobile}"></i>`)
+    );
   }
 }
 
@@ -15,18 +20,30 @@ function updateNavbarContent() {
 updateNavbarContent();
 
 // Run on window resize
-window.addEventListener('resize', updateNavbarContent);
+window.addEventListener("resize", updateNavbarContent);
 
-navContainer.addEventListener('mouseover', function(e) {
+navContainer.addEventListener("mouseover", function (e) {
   if (window.innerWidth > 600) return; // do nothing on large screens
-  const link = e.target.closest('.nav__link');
-  if(!link) return;
+  const link = e.target.closest(".nav__link");
+  if (!link) return;
   link.innerHTML = link.dataset.content;
 });
 
-navContainer.addEventListener('mouseout', function(e) {
+navContainer.addEventListener("mouseout", function (e) {
   if (window.innerWidth > 600) return; // do nothing on large screens
-  const link = e.target.closest('.nav__link');
-  if(!link) return;
-  link.innerHTML = link.innerHTML = `<i class="${link.dataset.content__mobile}"></i>`;
+  const link = e.target.closest(".nav__link");
+  if (!link) return;
+  link.innerHTML =
+    link.innerHTML = `<i class="${link.dataset.content__mobile}"></i>`;
+});
+
+// Hero Section
+window.addEventListener("load", function () {
+  namePopup.classList.remove("hidden");
+  namePopup.classList.add("show");
+  this.setTimeout(() => {
+    namePopup.classList.add("hidden");
+    namePopup.classList.remove("show");
+    homeIntro.classList.add("show");
+  }, 2000);
 });
